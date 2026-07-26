9267bd45a27c07844f085c3ac93ab8ad5816b732
nacimoualla <ouallanacim9@gmail.com>
2026-07-26 20:45:23 +0100

    feat: add public API exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export {};
+export type {
+  TimingInfo,
+  RequestCapture,
+  ResponseCapture,
+  DebugEntry,
+  MiddlewareOptions,
+} from './types.js';
+
+export { createTiming } from './core/timing.js';
+export { generateId, captureRequestBody, captureResponseBody } from './core/capture.js';
+export { formatEntry } from './core/formatter.js';
+export { sanitizeHeaders } from './core/sanitize.js';
